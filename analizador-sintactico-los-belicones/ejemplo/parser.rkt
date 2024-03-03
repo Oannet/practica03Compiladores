38462b3ea2684fae7fb67c314ed8f1076a7ce2c5
Kevin-Antonio-Carvajal <Kevin-Antonio@ciencias.unam.mx>
2024-03-03 17:14:14 -0600

    Versión 6

--- analizador-sintactico-los-belicones/ejemplo/parser.rkt
+++ analizador-sintactico-los-belicones/ejemplo/parser.rkt
@@ -1,50 +1,98 @@
-(provide contenedores
-         vacios
-         jelly-lexer)
+#lang nanopass
+(require "lexer.rkt"
+          parser-tools/yacc)
 
-#|
-Definimos las estructuras y expresiones con sus 
-respectivos cuerpos dentro de ellas para que trabajen
-como nodos en el árbol.
-|#
-(define-struct bin-exp (op arg1 arg2) #:transparent)
-(define-struct sin-exp (op arg1) #:transparent)
-(define-struct number (n) #:transparent)
-(define-struct bool (n) #:transparent)
-(define-struct id (i) #:transparent)
-(define-struct decl-var (i type) #:transparent)
-(define-struct array (type) #:transparent)
-(define-struct decl-arr (i type args) #:transparent)
-(define-struct length (var) #:transparent)
-(define-struct int-type ( ) #:transparent)
-(define-struct bool-type ( ) #:transparent)
-(define-struct fun (id  args body) #:transparent)
-(define-struct if-exp (conditional yes no) #:transparent)
-(define-struct if-short-exp (declaration conditional yes no) #:transparent)
-(define-struct while-exp (conditional body) #:transparent)
-(define-struct funcion-expr (id args type body) #:transparent)
-(define-struct funcall (id args) #:transparent)
-(define-struct main (exp func-list) #:transparent)
-(define-struct program (m met) #:transparent)
+(define-struct array-access (id index) #:transparent)
+(define-struct assign-add (id expr) #:transparent)
+(define-struct assign-sub (id expr) #:transparent)
+(define-struct inc (id) #:transparent)
+(define-struct dec (id) #:transparent)
+(define-struct multiple-decl (vars) #:transparent)
+
+; ESTRUCTURAS PARA LOS NODOS DEL ARBOL
+(define-struct id (i) #:transparent) 
+(define-struct num (n) #:transparent)
+(define-struct bool (b) #:transparent)
 (define-struct return (exp) #:transparent)
+(define-struct length (exp) #:transparent)
+(define-struct arg (id tipo) #:transparent)
+(define-struct un-expr (op e) #:transparent)
+(define-struct if-corto (g t e) #:transparent)                
+(define-struct call (nombre arg) #:transparent)
+(define-struct my-while (exp then) #:transparent)
+(define-struct bin-expr (op o1 o2) #:transparent)
+(define-struct main (instrucciones) #:transparent)
+(define-struct my-if (exp then else) #:transparent)
+(define-struct arreglo (id tipo tamano) #:transparent)           
+(define-struct procedimiento (nombre argumentos tipo instrucciones) #:transparent)
 
 (define jelly-parser
   (parser
-    [start program]    ; Simbolo inicial de la gramatica
-    [end EOF]       ; Paramos cuando veamos el token EOF
-    [tokens contenedores vacios] ; Tokens que reconocerá el parser
-    [error void]    ; Procedimiento si encontramos un error
-
-    [precs  (nonassoc NOT)
-            (left * / MOD)
-            (left + -)
-            (left <= >= < >)
-            (left = EQ NEQ)
+    [start program]    
+    [end EOF]       
+    [tokens contenedores vacios] 
+    [error void]    
+    
+    [precs  (right EQ += -= *= /=) 
+            (nonassoc NEQ < <= > >=)
+            (left ADD -)
+            (left MUL / MOD) 
+            (nonassoc NOT)
             (left AND)
             (left OR)]
-    
+
     [grammar
         [program
-            [(main) (program $1 void)]]
+            [(main func-list) (main $1 $2)]]
         [main
-            [(MAIN LBr expr RBr expr-list) (main $3 $5)]]
+            [(MAIN LBr decl-var-list expr RBr) (main $3 $4)]]
+        [func-list
+            [() '()]
+            [(func func-list) (cons $1 $2)]]
+        [func
+            [(ID LP arg-list RP type LBr expr RBr) (procedimiento $1 $3 $5 $7)]]
+        [arg-list
+            [() '()]
+            [(ID) (list (arg $1))]
+            [(ID COM arg-list) (cons (arg $1) $3)]]
+        [type
+            [(INT) 'INT]
+            [(BOOL) 'BOOL]]
+        [decl-var-list
+            [() '()]
+            [(decl-var decl-var-list) (cons $1 $2)]]
+        [expr
+            [(ID) (id $1)]
+            [(NUM) (num $1)]
+            [(expr ADD expr) (bin-expr '+ $1 $3)]
+            [(expr SUB expr) (bin-expr '- $1 $3)]
+            [(expr MUL expr) (bin-expr '* $1 $3)]
+            [(expr DIV expr) (bin-expr '/ $1 $3)]
+            [(expr MOD expr) (bin-expr '% $1 $3)]
+            [(NOT expr) (un-expr 'NOT $2)]
+            [(expr AND expr) (bin-expr 'AND $1 $3)]
+            [(expr OR expr) (bin-expr 'OR $1 $3)]
+            [(expr EQ expr) (bin-expr 'EQ $1 $3)]
+            [(expr NEQ expr) (bin-expr 'NEQ $1 $3)]
+            [(expr LT expr) (bin-expr '< $1 $3)]
+            [(expr LE expr) (bin-expr '<= $1 $3)]
+            [(expr GT expr) (bin-expr '> $1 $3)]
+            [(expr GE expr) (bin-expr '>= $1 $3)]
+            [(ID LBr expr RBr) (array-access $1 $3)]
+            [(LEN LP ID RP) (length $3)]
+            [(ID ADD= expr) (assign-add $1 $3)]
+            [(ID SUB= expr) (assign-sub $1 $3)]
+            [(ID INC) (inc $1)]
+            [(ID DEC) (dec $1)]
+            [(ID COM ID COM ID EQ expr) (multiple-decl (list $1 $3 $5 $7))]
+        ]
+    ]
+  )
+)
+
+
+(define (lex-this lexer input) (lambda () (lexer input)))
+
+(define (parsea in)
+        (let ([in-s (open-input-string in)])
+        (hola-parser (lex-this jelly-lex in-s))))
